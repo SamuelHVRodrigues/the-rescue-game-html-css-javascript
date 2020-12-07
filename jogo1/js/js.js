ab82d68276cede4c32845f611cef82b1a3449ee5
SamuelHVRodrigues <samuelhvrodrigues>
2020-12-07 04:15:37 -0300

    add files, interface and character animation

---
+++ jogo1/js/js.js
@@ -0,0 +1,8 @@
+function start() {
+    $("#inicio").hide();
+
+    $("#fundo-game").append("<div id='jogador' class='anima-jogador'></div>")
+    $("#fundo-game").append("<div id='inimigo1' class='anima-inimigo1'></div>")
+    $("#fundo-game").append("<div id='inimigo2'></div>")
+    $("#fundo-game").append("<div id='amigo' class='anima-amigo'></div>")
+}
